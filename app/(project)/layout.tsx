11c56389d2dd1338461f5bac68bbdca0dcafe5aa
Allan Francis <allantecnologo@gmail.com>
2025-04-14 17:56:26 -0400

    remove unused assets and update layout with Tailwind CSS

---
+++ app/(project)/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Template Saas",
+  description: "Template para criação de aplicações SaaS",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${poppins.className} bg-gray-200 antialiased`}
+        cz-shortcut-listen="true"
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
